dbeafbd7dfad8487b454e333d7bd031d878cdd7b
octopus8848 <zyl2954351566@126.com>
2024-11-02 17:30:07 +0800

    Site updated: 2024-11-02 17:30:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/18/hello-world/"];function toRandomPost(){
+var posts=["2024/11/01/11-1今日美食/","2024/11/05/10-31今日美食/","2024/11/03/11-2/","2024/11/05/111/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
